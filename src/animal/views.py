20ea8e5b6321a548b80bb8e756a1ffe250473d89
Naing Grace <grvce.n@gmail.com>
2025-12-20 16:19:23 +0100

    Code:  added simple animal list with hyperlinks to SOAPNote

--- src/animal/views.py
+++ src/animal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Animal, SOAPNote
 
-# Create your views here.
+
+def animal_list(request):
+    animals = Animal.objects.all()
+    return render(request, 'animal/animal_list.html', {'animals': animals})
+
+
+def display_animal_note(request, pk):
+    try:
+        if pk:
+            animal_note = SOAPNote.objects.get(animal__pk=pk)
+    except SOAPNote.DoesNotExist:
+        animal_note = None
+
+    return render(request, 'animal/animal_note.html', {'animal_note': animal_note})
