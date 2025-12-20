9ed7fef03992df7f8e77f285d7190963503a48a2
Naing Grace <grvce.n@gmail.com>
2025-12-20 17:07:34 +0100

    Code: added inventory view

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Inventory
 
-# Create your views here.
+
+def inventory_list(request):
+    inventory = Inventory.objects.all()
+    return render(request, 'inventory/inventory_list.html', {'inventory': inventory})
