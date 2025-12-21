c63de0a588819387b8d2fe4ab2fa7bdbd06b4284
Naing Grace <grvce.n@gmail.com>
2025-12-21 16:41:09 +0100

    Code: added function to insert data with the popup in views.py (still need testing)

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inventory_list, name='inventory_list'),
+    path('add_item/', views.add_item, name='add_item'),
 ]
--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from .models import Inventory
+from .models import Inventory, Item, TreatmentType
 
 
 def inventory_list(request):
     inventory = Inventory.objects.all()
     return render(request, 'inventory/inventory_list.html', {'inventory': inventory})
+
+
+def add_item(request):
+    all_types = TreatmentType.objects.all()
+    inventory = Inventory.objects.all()
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        reminder = request.POST.get('reminder')
+        price = request.POST.get('price')
+        treatment_type = request.POST.get('treatment_type')
+        Item.objects.create(
+            name=name, reminder=reminder, price=price, treatment_type=treatment_type
+        )
+
+        quantity = request.POST.get('quantity')
+        expiration_date = request.POST.get('expiration_date')
+        Inventory.objects.create(
+            item=Item.objects.last(), quantity=quantity, expiration_date=expiration_date
+        )
+
+    return render(
+        request,
+        'inventory/inventory_list.html',
+        {'inventory': inventory, 'all_types': all_types},
+    )
