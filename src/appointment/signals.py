91875932c4d4ae46d48e31c79c5e07ec5ccf9aff
Naing Grace <grvce.n@gmail.com>
2026-01-03 11:13:06 +0100

    Code: working dynamic inventory management (with signals.py)

---
+++ src/appointment/signals.py
@@ -0,0 +1,57 @@
+from django.db.models.signals import post_save, post_delete, pre_save
+from django.dispatch import receiver
+from .models import AppointmentItem
+from inventory.models import Item, Inventory
+
+
+@receiver(pre_save, sender=AppointmentItem)
+def store_old_quantity(sender, instance, **kwargs):
+    """
+    Stocke l'ancienne quantité avant la sauvegarde
+    """
+    if instance.pk:  # Si l'objet existe déjà
+        try:
+            old_instance = AppointmentItem.objects.get(pk=instance.pk)
+            instance._old_quantity = old_instance.quantity
+        except AppointmentItem.DoesNotExist:
+            instance._old_quantity = 0
+    else:
+        instance._old_quantity = 0
+
+
+@receiver(post_save, sender=AppointmentItem)
+def update_inventory_on_save(sender, instance, created, **kwargs):
+    """
+    Décrémente l'inventaire quand un item est ajouté ou modifié
+    """
+    item = instance.item
+    inventory_item = Inventory.objects.get(item=item)
+
+    if created:
+        # Nouvel item créé - décrémenter l'inventaire
+        difference = instance.quantity
+    else:
+        # Item modifié - ajuster l'inventaire
+        # Récupérer l'ancienne valeur depuis la base de données
+        old_quantity = getattr(instance, '_old_quantity', 0)
+        difference = instance.quantity - old_quantity
+
+    if inventory_item.quantity >= difference:
+        inventory_item.quantity -= difference
+        inventory_item.save()
+    else:
+        raise ValueError(
+            f"Stock insuffisant pour {item.name}. Disponible: {inventory_item.quantity}"
+        )
+
+
+# not sure if needed
+@receiver(post_delete, sender=AppointmentItem)
+def restore_inventory_on_delete(sender, instance, **kwargs):
+    """
+    Restaure l'inventaire quand un item est supprimé
+    """
+    item = instance.item
+    inventory_item = Inventory.objects.get(item=item)
+    inventory_item.quantity += instance.quantity
+    inventory_item.save()
