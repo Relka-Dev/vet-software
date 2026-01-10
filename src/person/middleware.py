c2f1c5538c6c7894f54e88c1e81f2b46937d16b3
Karel Vilém Svoboda <relka.dev@gmail.com>
2026-01-10 14:57:18 +0100

    fix: found another issue, when you tried to access the admin console as a person, it would result in an error -> auto logout

--- src/person/middleware.py
+++ src/person/middleware.py
@@ -8,15 +8,21 @@ class PersonAuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Exclusion des sites suivants pour garder la connexion admin
-        excluded_paths = ['/admin', '/favicon.ico', '/static/', '/media/']
+        excluded_paths = ['/favicon.ico', '/static/', '/media/']
 
         if any(request.path.startswith(path) for path in excluded_paths):
+            return self.get_response(request)
+
+        # Si on est sur l'admin et que l'utilisateur est une Person, logout
+        if request.path.startswith('/admin'):
+            if request.user.is_authenticated and isinstance(request.user, Person):
+                logout(request)
+                # return redirect('person:login')
             return self.get_response(request)
 
         # Pour le reste du site, on check si l'utilisateur est connecté en tant que instance de person. Sinon, logout.
         if request.user.is_authenticated and not isinstance(request.user, Person):
             logout(request)
-            # return redirect('person:login') # Si on veut la redirection si la personn est pas connectée
+            # return redirect('person:login') # Si on veut la redirection si la personne est pas connectée
 
         return self.get_response(request)
