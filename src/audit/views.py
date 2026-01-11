364094e7dd8bfbeb011b3d365c0ca526edc4e41f
Aurelie <phaurelie@msn.com>
2026-01-11 11:32:00 +0100

    added pagination

--- src/audit/views.py
+++ src/audit/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from audit.models import ActivityLog
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 @login_required
 def audit_logs(request):
     """Affiche les logs d'audit"""
-    logs = ActivityLog.objects.all()
+    logs_list = ActivityLog.objects.all().order_by('-timestamp')
+    paginator = Paginator(logs_list, 10)
+    page_number = request.GET.get('page')
+    logs = paginator.get_page(page_number)
     return render(request, 'audit/logs_list.html', {'logs': logs})
